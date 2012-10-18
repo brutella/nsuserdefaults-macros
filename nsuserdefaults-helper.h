6c21ac45c608d5fffe05f682ae4a1c98a642c69f
mah <matthias.hochgatterer@runtastic.com>
2012-10-18 12:38:52 +0200

    Change method defaults_observe_object to defaults_observe Post notification also when key is removed from the user defaults

--- nsuserdefaults-helper.h
+++ nsuserdefaults-helper.h
@@ -20,7 +20,8 @@ SI void defaults_save()                     		{ [defaults() synchronize]; }
 SI id defaults_object(NSString *key)         { return [defaults() objectForKey:key]; }
 
 SI void defaults_post_notification(NSString *key) {
-	[[NSNotificationCenter defaultCenter] postNotificationName:key object:nil userInfo:@{ @"value" : defaults_object(key) }];
+    id object = defaults_object(key);
+	[[NSNotificationCenter defaultCenter] postNotificationName:key object:nil userInfo: object ? @{ @"value" : object } : @{}];
 }
 
 SI void defaults_set_object(NSString *key, NSObject *object) {
@@ -29,11 +30,12 @@ SI void defaults_set_object(NSString *key, NSObject *object) {
 	defaults_post_notification(key);
 }
 
-SI void defaults_remove(NSString *key) { [defaults() removeObjectForKey:key]; }
+SI void defaults_remove(NSString *key) {
+    [defaults() removeObjectForKey:key];
+    defaults_post_notification(key);
+}
 
-typedef void(^ValueChangedBlock)(id object);
-
-SI void defaults_observe_object(NSString *key, ValueChangedBlock block) {
+SI void defaults_observe(NSString *key, void (^block) (id object)) {
 	[[NSNotificationCenter defaultCenter] addObserverForName:key object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *n){
 		block( [n.userInfo objectForKey:@"value"] );
 	}];
